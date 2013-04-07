21e8ada31083b6606ace5638e7c9a6972e8316aa
Anthony Gentile <asgentile@gmail.com>
2013-04-06 22:14:59 -0400

    Initial directory structure and example/stub files for the time being

---
+++ D2.cmd
@@ -0,0 +1,9 @@
+universe                = vanilla
+executable              = /usr/bin/python
+log                     = /tmp/condor.log
+error                   = /tmp/condor.error
+output                  = /tmp/condor.out
+arguments               = "src/D2.py"
+transfer_executable     = false
+getenv                  = true
+queue
