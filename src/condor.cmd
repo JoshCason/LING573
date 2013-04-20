389f406466613761e4cf30ec5a1ce0249994d058
Anthony Gentile <asgentile@gmail.com>
2013-04-19 20:35:44 -0700

    updated condor cmd file

--- src/condor.cmd
+++ src/condor.cmd
@@ -1,9 +1,9 @@
 universe                = vanilla
-executable              = /usr/bin/python
+executable              = /opt/python-2.7/bin/python2.7
 log                     = condor.log
 error                   = condor.error
 output                  = condor.out
-arguments               = "src/trec_questions.py /dropbox/12-13/573/Data/Questions/training/TREC-2005.xml"
+arguments               = "D2.py /dropbox/12-13/573/Data/Questions/devtest/TREC-2006.xml"
 transfer_executable     = false
 getenv                  = true
 queue
